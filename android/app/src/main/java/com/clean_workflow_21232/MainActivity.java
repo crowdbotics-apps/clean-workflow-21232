8dc43048401d165ff873f004797e7a47abcc7d32
Crowdbotics <team@crowdbotics.com>
2020-10-07 20:06:17 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/clean_workflow_21232/MainActivity.java
@@ -0,0 +1,15 @@
+package com.clean_workflow_21232;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "clean_workflow_21232";
+    }
+}
